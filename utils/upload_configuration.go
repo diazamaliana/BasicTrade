8d9bbf3d8ca640ddcb196c6e5fa79c53a5c93db4
Diaz Amaliana <diazamaliana@gmail.com>
2023-11-22 17:30:30 +0700

    master] [Basic Trade] set up cloudinary configuration

---
+++ utils/upload_configuration.go
@@ -0,0 +1,72 @@
+package utils
+
+import (
+	"basictrade/helpers"
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"path"
+	"time"
+
+	"github.com/cloudinary/cloudinary-go/v2"
+	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
+)
+
+func UploadFile(fileHeader *multipart.FileHeader, fileName string) (string, error) {
+	c, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Add Cloudinary product environment credentials.
+	cld, err := cloudinary.NewFromParams(helpers.EnvCloudName(), helpers.EnvCloudAPIKey(), helpers.EnvCloudAPISecret())
+	if err != nil {
+		return "", err
+	}
+	fmt.Println(helpers.EnvCloudName())
+
+	// Convert file
+	fileReader, err := convertFile(fileHeader)
+	if err != nil {
+		return "", err
+	}
+
+	// Upload file
+	uploadParam, err := cld.Upload.Upload(c, fileReader, uploader.UploadParams{
+		PublicID: fileName,
+		Folder:   helpers.EnvCloudUploadFolder(),
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return uploadParam.SecureURL, nil
+}
+
+func convertFile(fileHeader *multipart.FileHeader) (*bytes.Reader, error) {
+    if fileHeader == nil {
+        return nil, errors.New("file header is nil")
+    }
+
+    file, err := fileHeader.Open()
+    if err != nil {
+        return nil, err
+    }
+    defer file.Close()
+
+    // Read the file content into an in-memory buffer
+    buffer := new(bytes.Buffer)
+    if _, err := io.Copy(buffer, file); err != nil {
+        return nil, err
+    }
+
+    // Create a bytes.Reader from the buffer
+    fileReader := bytes.NewReader(buffer.Bytes())
+    return fileReader, nil
+}
+
+
+func RemoveExtension(filename string) string {
+	return path.Base(filename[:len(filename)-len(path.Ext(filename))])
+}
