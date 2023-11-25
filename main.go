58331277eb7b87ac440701b4f5e37376dbf489e8
Diaz Amaliana <diazamaliana@gmail.com>
2023-11-25 21:12:48 +0700

    [Basic Trade] update port configuration

--- main.go
+++ main.go
@@ -5,15 +5,21 @@ package main
 import (
 	"basictrade/routes"
 	database "basictrade/utils"
-
+	"os"
 )
 
-var (
-	PORT = ":5050"
-)
 
 func main() {
+	// Start the database connection
 	database.StartDB()
+
+	// Get the port from the environment variable or use a default value
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5050" // Default port if PORT environment variable is not set
+	}
+
+	// Start the application on the specified port
 	r := routes.StartApp()
-	r.Run(PORT)
+	r.Run(":" + port)
 }
